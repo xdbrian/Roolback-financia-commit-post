eccda59a6b7ac6e0e4ca647ae590414e3f3315c3
Brian Steventh Torres Yañez <btorresy@usersad.everis.int>
2017-02-21 09:48:42 -0500

    core- environment

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,13 @@
 import { NgModule }              from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
 import { OutlayComponent } from './module/outlay/outlay.component';
-import  { FecComponent } from './module/fec/fec.component'
+import  { FecComponent } from './module/fec/fec.component';
+import {} from './'
 const appRoutes: Routes = [
  //{path: 'financiar', component: FecComponent},
  //{ path: '',   redirectTo: '/financiar', pathMatch: 'full' },
  { path: 'desembolso', component:  OutlayComponent},
  { path: 'home', component:  OutlayComponent}
-
 ];
 
 @NgModule({
